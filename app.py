776270e10a2ef8283fadc1dcbcc427b98c9363a0
Fawaz Abid <53698026+fauwara@users.noreply.github.com>
2020-12-12 23:46:58 +0400

    code now works for image of all dimensions and made code cleaner

--- app.py
+++ app.py
@@ -1,92 +1,71 @@
 from PIL import Image
 import numpy as np
 
-# raw data of image
-# this np object is not an actuall object
-im_raw = np.array(Image.open('lmao.png'))
+##################################### CONFIG
+# image file
+image_file = 'lmao.png'
+# dimensions
+keep_aspect_ratio = False
+dimensions = (40,40) # (width, height)
+# testing
+display_image = False 
 
-print(f'------------------------------ image details ------------------------------')
-print(f'array dimensions: {im_raw.ndim}')
-print(f'image pixel height: {len(im_raw)}')
-print(f'image pixel width: {len(im_raw[0])}')
-print(f'---------------------------------------------------------------------------')
+##################################### SOME FUNCTIONS
+# calculates avg value of rgb of a single pixel.
+def compute_avg_rgb(pixel):
+    return (int(pixel[0]) + int(pixel[1]) + int(pixel[2]))/3
 
-# print(im_raw)
-im_height = len(im_raw)
-im_width = len(im_raw[0])
+# sets the colour of a specific pixel.
+def set_pixel_colour(pixel,rgb):
+    for i in range(3):
+        pixel[i] = rgb[i]
 
-px = 50
-count = 0
+##################################### INIT RESULT
+ascii_image = []
 
-pixel_comp_h = int(im_height/px)*2
 
-pixel_comp_w = int(im_width/px)
+# opening the image
+image = Image.open(image_file)
+# resizing the image
+if keep_aspect_ratio:
+    image = image.thumbnail(dimensions)
+else:
+    image = image.resize(dimensions)
 
-start_index_h = 0
-end_index_h = int(im_height/px)
+# convert image into raw (numpy array) format
+image_raw = np.array(image)
 
-start_index_w = 0
-end_index_w = int(im_width/px)
-
-row_list = []
-colomn_list = []
-
-for i in range(0,im_width,pixel_comp_w):
-    row_list.append([i,i+pixel_comp_w])
-
-b = []
-
-for i in range(0,im_height,pixel_comp_h):
-    c = [i,i+pixel_comp_h]
-    b.append([c,row_list])
-
-# b = [[[0,end_index_h],row_list],[[end_index_h,end_index_h*2],row_list]]
-
-res = []
-
-for i in b:
-    for j in range(len(row_list)):
-        # print(j)
-        avg_r = 0
-        avg_g = 0
-        avg_b = 0
-        res.append([])
-
-        for row_index in range(i[0][0],i[0][1]):
-            avg_row_r = 0
-            avg_row_g = 0
-            avg_row_b = 0
-
-            for pixel_index in range(i[1][j][0],i[1][j][1]):
-                avg_row_r += im_raw[row_index,pixel_index,0]
-                avg_row_g += im_raw[row_index,pixel_index,1]
-                avg_row_b += im_raw[row_index,pixel_index,2]
-
-            avg_r += int(avg_row_r/end_index_w)
-            avg_g += int(avg_row_g/end_index_w)
-            avg_b += int(avg_row_b/end_index_w)
-
-        rgb_value = int(int((avg_r/pixel_comp_h) + int(avg_g/pixel_comp_h) + int(avg_b/pixel_comp_h))/3)
-        # print(rgb_value)
-
-        if rgb_value < 30:
-            res[j].append('█')
-        elif rgb_value < 135:
-            res[j].append('▓')
-        elif rgb_value < 170:
-            res[j].append('▒')
-        elif rgb_value < 200:
-            res[j].append('░') 
-        elif rgb_value < 240:
-            res[j].append('.')
+row_index = 0
+for row in image_raw:
+    # enter into the row.
+    ascii_image.append([])
+    for pixel in row:
+        # enter into the pixel.
+        avg_rgb = compute_avg_rgb(pixel)
+        if avg_rgb < 30:
+            ascii_image[row_index].append('█')
+        elif avg_rgb < 135:
+            ascii_image[row_index].append('▓')
+        elif avg_rgb < 170:
+            ascii_image[row_index].append('▒')
+        elif avg_rgb < 200:
+            ascii_image[row_index].append('░')
+        elif avg_rgb < 240:
+            ascii_image[row_index].append('.')
         else: 
-            res[j].append(' ')                  
-            #im_raw[row_index,pixel_index,0] = 0
-        #im_raw[row_index,pixel_index,1] = 0
-        #im_raw[row_index,pixel_index,2] = 0
-    # print(im_raw[row_index])
+            ascii_image[row_index].append(' ')
     
-for i in range(px):
-    for j in range(px):
-        print(res[j][i],end = '')
+    row_index += 1
+
+for row in ascii_image:
+    for ascii_key in row:
+        print(ascii_key,end='')
     print('')
+
+
+# just for testing
+if display_image:
+    # convert image into displayable format
+    im = Image.fromarray(image_raw)
+    # display image
+    im.show()
