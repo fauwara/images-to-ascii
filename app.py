d16bb6559a15fc922f9865f1c7c95ea21b50dd0c
Fawaz Abid <53698026+fauwara@users.noreply.github.com>
2020-12-14 23:29:54 +0400

    fixed bugs and made code clean.

--- app.py
+++ app.py
@@ -3,63 +3,90 @@ import numpy as np
 
 ##################################### CONFIG
 # image file
-image_file = 'k-on.jpg'
+image_file = 'lmao.png'
 # dimensions
+dimensions = (50,50) # (width, height) keep '/2' there. A charector is larger in height and smaller in width this is to compensate for that.
 keep_aspect_ratio = True
-dimensions = (130,150) # (width, height)
+
 # testing
-display_image = False 
+testing = True
+display_image_details = True
+display_image = False
+display_ascii_text_details = True
+display_ascii_text = True
 
 ##################################### SOME FUNCTIONS
 # calculates avg value of rgb of a single pixel.
 def compute_avg_rgb(pixel):
     return (int(pixel[0]) + int(pixel[1]) + int(pixel[2]))/3
 
-# sets the colour of a specific pixel.
+# sets the colour of a specific pixel. not used so far
 def set_pixel_colour(pixel,rgb):
     for i in range(3):
         pixel[i] = rgb[i]
 
 ##################################### INIT RESULT
-ascii_image = []
 
 # opening the image
+if testing:
+    print(f'> Opening {image_file}...')
+
 image = Image.open(image_file)
+
+if testing:
+    print(f'> Image details:')
+    print(f'> Image Dimensions: {image.size}\n')
+    print(f'> Resizing image [keep aspect ratio: {keep_aspect_ratio}]... ')
+
 # resizing the image
 if keep_aspect_ratio:
-    image.thumbnail(dimensions)
+    image.thumbnail(dimensions) # image.thumbnail modifies the image and returns nothing so im not storing it in a variable.
+    image = image.resize((dimensions[0],int(dimensions[1]/2)))
+    if testing:
+        print(f'> New dimensions: {image.size}')
 else:
-    image.resize(dimensions)
+    image = image.resize(dimensions) # this does not modify but returns a new object hence I am storing it in a variable.
+    if testing:
+        print(f'> New dimensions: {image.size}')
 
 # convert image into raw (numpy array) format
 image_raw = np.array(image)
 
+ascii_text = []
+
 row_index = 0
 for row in image_raw:
     # enter into the row.
-    ascii_image.append([])
+    ascii_text.append([])
     for pixel in row:
         # enter into the pixel.
         avg_rgb = compute_avg_rgb(pixel)
         if avg_rgb < 30:
-            ascii_image[row_index].append('█')
+            ascii_text[row_index].append('█')
         elif avg_rgb < 135:
-            ascii_image[row_index].append('▓')
+            ascii_text[row_index].append('▓')
         elif avg_rgb < 170:
-            ascii_image[row_index].append('▒')
+            ascii_text[row_index].append('▒')
         elif avg_rgb < 200:
-            ascii_image[row_index].append('░')
+            ascii_text[row_index].append('░')
         elif avg_rgb < 240:
-            ascii_image[row_index].append('.')
-        else: 
-            ascii_image[row_index].append(' ')
+            ascii_text[row_index].append('.')
+        else:
+            ascii_text[row_index].append(' ')
     
     row_index += 1
 
-for row in ascii_image:
-    for ascii_key in row:
-        print(ascii_key,end='')
-    print('')
+##################################### DISPLAY IMAGE DETAILS
+if display_ascii_text_details:
+    print(' - ASCII TEXT - ')
+    print(f'ascii text width: {len(ascii_text[0])}')
+    print(f'ascii text height: {len(ascii_text)}')
+
+if display_ascii_text:
+    for row in ascii_text:
+        for ascii_key in row:
+            print(ascii_key,end='')
+        print('')
 
 # just for testing
 if display_image:
